d6cb718fc6d321ecba04cd7b8c31f620e8daedb2
ShakalakaB <yunjie989@gmail.com>
2021-05-07 22:28:19 +0800

    Refract Service Interface to Common Bean Interface. close #11

--- pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/VetService.java
@@ -3,10 +3,5 @@ package aldora.springframework.springpetclinic.services;
 import aldora.springframework.springpetclinic.model.Vet;
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
