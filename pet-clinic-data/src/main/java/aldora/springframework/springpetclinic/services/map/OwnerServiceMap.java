0da0c672b2a8926e3ecdcc75651436d4e45dc21f
ShakalakaB <yunjie989@gmail.com>
2021-05-07 23:35:23 +0800

    Implement map based services. close #6

---
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package aldora.springframework.springpetclinic.services.map;
+
+import aldora.springframework.springpetclinic.model.Owner;
+import aldora.springframework.springpetclinic.services.CrudService;
+import aldora.springframework.springpetclinic.services.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
