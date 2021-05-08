974ff0a508abe0cf0f345fca1c35cefd906bc23b
ShakalakaB <yunjie989@gmail.com>
2021-05-08 21:24:45 +0800

    close #23

--- pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -23,26 +23,22 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Santa");
         owner1.setLastName("Uno");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Jack");
         owner2.setLastName("Ma");
         ownerService.save(owner2);
         System.out.println("owners created!");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("vet1");
         vet1.setLastName("com1");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("vet2");
         vet2.setLastName("com2");
         vetService.save(vet2);
