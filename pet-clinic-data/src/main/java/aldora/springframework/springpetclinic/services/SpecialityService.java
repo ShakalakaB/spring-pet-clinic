0a99f6cb842decf0a9387818e632d2ba42cee9c6
ShakalakaB <yunjie989@gmail.com>
2021-05-12 21:43:01 +0800

    Create specialty service. close #31

---
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package aldora.springframework.springpetclinic.services;
+
+import aldora.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
