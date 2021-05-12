2f0bc10cafb4e18b747c2dcd06fdd9da6010b950
ShakalakaB <yunjie989@gmail.com>
2021-05-12 21:08:48 +0800

    Enhance owners with contact info and pets. close #33

--- pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package aldora.springframework.springpetclinic.services.map;
 
 import aldora.springframework.springpetclinic.model.Owner;
+import aldora.springframework.springpetclinic.model.Pet;
 import aldora.springframework.springpetclinic.services.OwnerService;
+import aldora.springframework.springpetclinic.services.PetService;
+import aldora.springframework.springpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -20,6 +31,30 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
+        if (object == null) {
+            throw new RuntimeException("Object cannot be null");
+        }
+
+        if (object.getPets() == null) {
+            return super.save(object);
+        }
+
+
+        object.getPets().forEach(pet -> {
+            if (pet.getPetType() == null) {
+                throw new RuntimeException("Pet type is required");
+            }
+
+            if (pet.getPetType().getId() == null) {
+                pet.setPetType(petTypeService.save(pet.getPetType()));
+            }
+
+            if (pet.getId() == null) {
+                Pet petSaved = petService.save(pet);
+                pet.setId(petSaved.getId());
+            }
+        });
+
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package aldora.springframework.springpetclinic.bootstrap;
 
 import aldora.springframework.springpetclinic.model.Owner;
+import aldora.springframework.springpetclinic.model.Pet;
 import aldora.springframework.springpetclinic.model.PetType;
 import aldora.springframework.springpetclinic.model.Vet;
 import aldora.springframework.springpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import aldora.springframework.springpetclinic.services.PetTypeService;
 import aldora.springframework.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -34,12 +37,27 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Santa");
         owner1.setLastName("Uno");
+        owner1.setAddress("Avenue1");
+        owner1.setCity("HK");
+        owner1.setTelephone("123123");
+
+        Pet petMik = new Pet();
+        petMik.setName("Mika");
+        petMik.setPetType(dogPetType);
+        petMik.setBirthDate(LocalDate.now());
+        petMik.setOwner(owner1);
+
+        owner1.getPets().add(petMik);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Jack");
         owner2.setLastName("Ma");
+        owner2.setAddress("avenue2");
+        owner2.setCity("Macau");
+        owner2.setTelephone("312121");
         ownerService.save(owner2);
+
         System.out.println("owners created!");
 
         Vet vet1 = new Vet();
