1fbc296d443fdfaafca228c1e268712ac5c2f425
ShakalakaB <yunjie989@gmail.com>
2021-05-12 16:27:44 +0800

    Create pettype service. close #29

---
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package aldora.springframework.springpetclinic.services;
+
+import aldora.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
