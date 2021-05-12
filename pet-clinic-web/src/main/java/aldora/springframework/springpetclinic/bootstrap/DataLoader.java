73ddb4ea8ebc448facc88237c84a2ddb24e072cf
ShakalakaB <yunjie989@gmail.com>
2021-05-12 16:37:55 +0800

    Add pettype in with bootstrap(startup). close #32

--- pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,27 +1,36 @@
 package aldora.springframework.springpetclinic.bootstrap;
 
 import aldora.springframework.springpetclinic.model.Owner;
+import aldora.springframework.springpetclinic.model.PetType;
 import aldora.springframework.springpetclinic.model.Vet;
 import aldora.springframework.springpetclinic.services.OwnerService;
+import aldora.springframework.springpetclinic.services.PetTypeService;
 import aldora.springframework.springpetclinic.services.VetService;
-import aldora.springframework.springpetclinic.services.map.OwnerServiceMap;
-import aldora.springframework.springpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
-
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dogPetType = new PetType();
+        dogPetType.setName("Dog");
+        petTypeService.save(dogPetType);
+
+        PetType catPetType = new PetType();
+        catPetType.setName("Cat");
+        petTypeService.save(catPetType);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Santa");
         owner1.setLastName("Uno");
