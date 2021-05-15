2b0b15751a2676bfd3790abdb3a59f4368a0ad43
ShakalakaB <yunjie989@gmail.com>
2021-05-15 23:11:45 +0800

    Convert Visit to jpa entity. close #44

--- pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/model/Pet.java
@@ -1,10 +1,10 @@
 package aldora.springframework.springpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -18,7 +18,11 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +54,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
