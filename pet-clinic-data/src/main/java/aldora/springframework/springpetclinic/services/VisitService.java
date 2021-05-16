16694d45931c3314bdb3e71e468b6c877576d5e0
ShakalakaB <yunjie989@gmail.com>
2021-05-16 14:50:10 +0800

    Create visit map service. close #45

---
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package aldora.springframework.springpetclinic.services;
+
+import aldora.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
--- pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/controllers/OwnerController.java
@@ -1,7 +1,6 @@
 package aldora.springframework.springpetclinic.controllers;
 
 import aldora.springframework.springpetclinic.services.OwnerService;
-import aldora.springframework.springpetclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
