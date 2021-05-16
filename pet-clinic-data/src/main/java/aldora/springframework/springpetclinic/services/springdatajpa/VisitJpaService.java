39d667c2f6e2bb50ab7bb88d61bd912332061306
ShakalakaB <yunjie989@gmail.com>
2021-05-16 15:14:24 +0800

    Load visit on startup in bootstrap, add visit jpa service. close #46

---
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/services/springdatajpa/VisitJpaService.java
@@ -0,0 +1,48 @@
+package aldora.springframework.springpetclinic.services.springdatajpa;
+
+import aldora.springframework.springpetclinic.model.Visit;
+import aldora.springframework.springpetclinic.repositories.VisitRepository;
+import aldora.springframework.springpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/aldora/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package aldora.springframework.springpetclinic.bootstrap;
 
 import aldora.springframework.springpetclinic.model.*;
-import aldora.springframework.springpetclinic.services.OwnerService;
-import aldora.springframework.springpetclinic.services.PetTypeService;
-import aldora.springframework.springpetclinic.services.SpecialityService;
-import aldora.springframework.springpetclinic.services.VetService;
+import aldora.springframework.springpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,13 +13,16 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialityService specialityService) {
+                      SpecialityService specialityService,
+                      VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -66,6 +66,12 @@ public class DataLoader implements CommandLineRunner {
 
         owner1.getPets().add(petMik);
         ownerService.save(owner1);
+
+        Visit visit = new Visit();
+        visit.setDate(LocalDate.now());
+        visit.setDescription("Kitty");
+        visit.setPet(petMik);
+        visitService.save(visit);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Jack");
