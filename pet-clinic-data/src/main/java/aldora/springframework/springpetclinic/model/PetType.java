fd3230d072d3538e39b0a15f64ad74b929ac69f4
ShakalakaB <yunjie989@gmail.com>
2021-05-16 22:28:59 +0800

    Add project lombox and refract. close #49

--- pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/aldora/springframework/springpetclinic/model/PetType.java
@@ -1,18 +1,17 @@
 package aldora.springframework.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pet_types")
 public class PetType extends BaseEntity {
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
